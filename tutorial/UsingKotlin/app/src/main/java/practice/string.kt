4f74bf76878e118d365ff8de78b7054681644ae0
syu-kwsk <wataru1165kwsk@gmail.com>
2020-08-13 09:19:24 +0900

    Compare variable

--- tutorial/UsingKotlin/app/src/main/java/practice/string.kt
+++ tutorial/UsingKotlin/app/src/main/java/practice/string.kt
@@ -19,4 +19,14 @@ fun main() {
               nice Kotlin!
     """.trimIndent() // インデントが小さいやつに合わせる
     println(multiline3)
+
+    var str = "Kotlin"
+    if (str == "Kotlin") println("Hello, Kotlin!")
+    else if (str == "Java") println("Hello, Java!")
+
+    val a = Integer(12)
+    val b = Integer(12)
+    println(a === b) // オブジェクト比較
+    println(a == b) // 値比較
+
 }
