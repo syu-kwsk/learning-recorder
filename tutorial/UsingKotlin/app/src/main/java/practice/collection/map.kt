981373f1c6994105aa7096348f33c8dcd45e02d7
syu-kwsk <wataru1165kwsk@gmail.com>
2020-09-08 08:49:16 +0900

    Practice immutable map collection

---
+++ tutorial/UsingKotlin/app/src/main/java/practice/collection/map.kt
@@ -0,0 +1,10 @@
+package practice.collection
+
+fun main() {
+//    mapはkey&value, keyは重複なし
+//    mutableとimmutable
+    val immutableFruits: Map<String, Int> = mapOf("apple" to 1, "orange" to 2, "banana" to 3)
+    print(immutableFruits)
+    println("")
+
+}
