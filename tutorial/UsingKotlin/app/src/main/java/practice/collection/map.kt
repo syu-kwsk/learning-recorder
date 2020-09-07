8a541c9f62acabaf7e74b5cd41b2d6e0355cf056
syu-kwsk <wataru1165kwsk@gmail.com>
2020-09-08 08:56:36 +0900

    Practice mutable map collection

--- tutorial/UsingKotlin/app/src/main/java/practice/collection/map.kt
+++ tutorial/UsingKotlin/app/src/main/java/practice/collection/map.kt
@@ -7,4 +7,23 @@ fun main() {
     print(immutableFruits)
     println("")
 
+//    mutable
+    val mutableFruits: MutableMap<String, Int> = mutableMapOf(
+        "apple" to 1,
+        "orange" to 2,
+        "banana" to 3
+    )
+    print(mutableFruits.get("apple")) //  Should be replaced with indexing
+    print(", ")
+    print(mutableFruits["orange"])
+    println("")
+//    add & remove element
+    mutableFruits.put(
+        "melon", 4
+    ) // Should be mutableFruits["melon"] = 4
+    println(mutableFruits)
+
+    mutableFruits.remove("banana")
+    println(mutableFruits)
+
 }
