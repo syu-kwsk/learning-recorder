5cc272ca6529ce056eea97b220cc593109a02970
syu-kwsk <wataru1165kwsk@gmail.com>
2020-09-09 08:49:14 +0900

    Practice constructor and initializer

--- tutorial/UsingKotlin/app/src/main/java/practice/classAndInterface/class.kt
+++ tutorial/UsingKotlin/app/src/main/java/practice/classAndInterface/class.kt
@@ -3,7 +3,9 @@ package practice.classAndInterface
 import java.nio.file.attribute.FileOwnerAttributeView
 
 fun main() {
-    val dog = Dog()
+    val dog = Dog("Black")
+    println(dog.color)
+
     dog.name = "Pochi"
     println(dog.name)
 //    use custom setter
@@ -15,7 +17,12 @@ fun main() {
 
 }
 
-class Dog {
+class Dog(val color: String) { // primary constructor
+//    initializer
+    init {
+        println("The dog's color is $color")
+    }
+
     var name: String = "" // if use val, it is read only property
 
 //    set double age
