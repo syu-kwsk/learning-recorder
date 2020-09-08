8374d7a1fda88dc91fad4a3c8a8236e468015696
syu-kwsk <wataru1165kwsk@gmail.com>
2020-09-09 08:33:57 +0900

    Practice custom setter

--- tutorial/UsingKotlin/app/src/main/java/practice/classAndInterface/class.kt
+++ tutorial/UsingKotlin/app/src/main/java/practice/classAndInterface/class.kt
@@ -4,9 +4,19 @@ fun main() {
     val dog = Dog()
     dog.name = "Pochi"
     println(dog.name)
+//    use custom setter
+    dog.age = 10
+    println(dog.age)
 
 }
 
 class Dog {
     var name: String = "" // if use val, it is read only property
+
+//    set double age
+//    parameter is called "field"
+    var age: Int = 0
+        set(value) {
+            field = value*2 // field = this.age
+        }
 }
