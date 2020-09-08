d00b34218c241b459614eeec7adaba4c1c61cb35
syu-kwsk <wataru1165kwsk@gmail.com>
2020-09-08 23:36:18 +0900

    Practice implicit it and functional argument

--- tutorial/UsingKotlin/app/src/main/java/practice/function/lambda.kt
+++ tutorial/UsingKotlin/app/src/main/java/practice/function/lambda.kt
@@ -15,4 +15,17 @@ fun main() {
     var minus3: (Int, Int) -> Int = {x, y -> x - y}
     println(minus(3, 1))
 
+//    use implicit "it"
+//    a arg can be used in lambda as "it"
+    var double: (Int)->Int = {it * 2}
+    println(double(5))
+//    same as
+    var double2: (Int)->Int = {x -> x*2}
+
+//    functional type as argument
+    fun doLambda(x: Int, l: (Int)->Int) = l(x)
+    println(doLambda(5) { it * 2 })
+//    change process
+    println(doLambda(4){ it * 10 })
+
 }
