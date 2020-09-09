bbd1a58c97285c79cdd022a44d0008a7426cc632
syu-kwsk <wataru1165kwsk@gmail.com>
2020-09-10 08:41:30 +0900

    Practice null safe

---
+++ tutorial/UsingKotlin/app/src/main/java/practice/otherFeatures/nullSafe.kt
@@ -0,0 +1,24 @@
+package practice.otherFeatures
+
+fun main() {
+//    安全呼び出し演算子 "?."
+//    objectがnullならnullが返される
+
+//    s is null
+    var s: String? = null
+    println("The length of $s is ${s?.length}.")
+//    s is "abc"
+    s = "abc"
+    println("The length of $s is ${s?.length}.")
+
+//    エルビス演算子 "?:"
+//    nullの場合にデフォルト値を返す
+    s = null
+    val t: String = s ?: "default"
+    println(t)
+//    nullable argument
+    fun lengthNullable(s: String?) = s?.length ?: 0
+    println(lengthNullable("abcd"))
+    println(lengthNullable(null))
+
+}
