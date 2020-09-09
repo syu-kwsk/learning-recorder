e741371898f520eed8992799f54ba24703c6c667
syu-kwsk <wataru1165kwsk@gmail.com>
2020-09-10 08:50:17 +0900

    ScopeFunction with

---
+++ tutorial/UsingKotlin/app/src/main/java/practice/otherFeatures/scopeFunctions.kt
@@ -0,0 +1,25 @@
+package practice.otherFeatures
+
+fun main() {
+//    スコープ関数
+//    変数のスコープを変更する
+
+//    "with" function
+//    without "with"
+    val dog = Dog("Pochi")
+    dog.age = 10
+    dog.message()
+//    with "with"
+    with(Dog("Pochi")) {
+        age = 10
+        println(message())
+    }
+
+}
+
+class Dog(val name: String) {
+    var age: Int = 0
+    fun message(): String {
+        return "Dog's name is $name, $age years old"
+    }
+}
