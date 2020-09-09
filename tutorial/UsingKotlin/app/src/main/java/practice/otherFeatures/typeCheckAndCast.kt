869d1300db9f86428fd317f07f3649b358c4c952
syu-kwsk <wataru1165kwsk@gmail.com>
2020-09-09 22:21:56 +0900

    Practice cast and as operator

--- tutorial/UsingKotlin/app/src/main/java/practice/otherFeatures/typeCheckAndCast.kt
+++ tutorial/UsingKotlin/app/src/main/java/practice/otherFeatures/typeCheckAndCast.kt
@@ -7,6 +7,29 @@ fun main() {
     var str: String = "one"
     println(getLen(str))
 
+//    use "as" operator
+    open class Person(val name: String) {}
+    class Student(name: String, var degree: String) : Person(name) {}
+
+    var person: Person = Student("Sato", "Bachelor")
+    var student: Student = person as Student // cast to Student
+    println(student.name)
+    println(student.degree)
+
+//    null can't cast
+//    var person2: Person? = null
+//    var student2 = person2 as Student
+
+//    irrelevant type can't cast
+//    var person3: Person = Student("Sato", "Bachelor")
+//    var student3 = person3 as Int
+
+//    use "as?"
+//    don't throw exception
+    var person4: Person = Student("Sato", "Bachelor")
+    var student4 = person4 as? Int
+    print(student4 is Int)
+
 }
 
 fun getLen(obj: Any): Int? {
