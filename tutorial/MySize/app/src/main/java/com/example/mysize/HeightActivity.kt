595353da4f6c2d0dbd2562e28123ba68ddd2c59f
syu-kwsk <wataru1165kwsk@gmail.com>
2020-09-10 23:35:04 +0900

    Select height by Spinner

--- tutorial/MySize/app/src/main/java/com/example/mysize/HeightActivity.kt
+++ tutorial/MySize/app/src/main/java/com/example/mysize/HeightActivity.kt
@@ -2,10 +2,34 @@ package com.example.mysize
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.Spinner
+import kotlinx.android.synthetic.main.activity_height.*
 
 class HeightActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_height)
+
+        spinner.onItemSelectedListener =
+            object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
+                    val spinner = parent as? Spinner
+                    val item = spinner?.selectedItem as? String
+                    item?.let {
+                        if (it.isNotEmpty()) height.text = it
+                    }
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+//                    TODO("not implemented")
+                }
+            }
     }
 }
