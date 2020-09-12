7a6686d296160f63137e38a0c73db4b59a6a2d4b
syu-kwsk <wataru1165kwsk@gmail.com>
2020-09-12 18:35:17 +0900

    Transaction to sub schreen

--- tutorial/AnimalBook/app/src/main/java/com/example/animalbook/MainActivity.kt
+++ tutorial/AnimalBook/app/src/main/java/com/example/animalbook/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.animalbook
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
@@ -10,5 +11,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val fragment = titleFragment as? TitleFragment
         fragment?.setTitle("フラグメント動物図鑑")
+
+        nextButton.setOnClickListener {
+            val intent = Intent(this, SubActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
