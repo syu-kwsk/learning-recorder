f3bb78468d900b94ca16af1b614b2840d81e106d
syu-kwsk <wataru1165kwsk@gmail.com>
2020-09-16 12:26:34 +0900

    Correspond deprecated SoundPool API

--- tutorial/Countdown/app/src/main/java/com/example/countdown/MainActivity.kt
+++ tutorial/Countdown/app/src/main/java/com/example/countdown/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.countdown
 
 import android.content.IntentSender
+import android.media.AudioAttributes
 import android.media.AudioManager
 import android.media.SoundPool
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
@@ -62,7 +64,19 @@ class MainActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
-        soundPool = SoundPool(2, AudioManager.STREAM_ALARM, 0)
+        soundPool = if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+            @Suppress("DEPRECATION")
+            SoundPool(2, AudioManager.STREAM_ALARM, 0)
+
+        } else {
+            val audioAttributes = AudioAttributes.Builder()
+                .setUsage(AudioAttributes.USAGE_ALARM)
+                .build()
+            SoundPool.Builder()
+                .setMaxStreams(1)
+                .setAudioAttributes(audioAttributes)
+                .build()
+        }
         soundResId = soundPool.load(this, R.raw.bellsound, 1)
     }
 
