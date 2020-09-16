004448390fc7e6c2e25d8bd9571fa19060623508
syu-kwsk <wataru1165kwsk@gmail.com>
2020-09-16 10:56:10 +0900

    Slide one page per 1sec

--- tutorial/Slideshow/app/src/main/java/com/example/slideshow/MainActivity.kt
+++ tutorial/Slideshow/app/src/main/java/com/example/slideshow/MainActivity.kt
@@ -2,12 +2,14 @@ package com.example.slideshow
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.concurrent.timer
 
 class MainActivity : AppCompatActivity() {
     class MyAdapter(fm: FragmentActivity) : FragmentStateAdapter(fm){
@@ -32,5 +34,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         pager.adapter = MyAdapter(this)
+        val handler = Handler()
+        timer(period = 1000) {
+            handler.post {
+                pager.currentItem = (pager.currentItem + 1) % 10
+            }
+        }
     }
 }
